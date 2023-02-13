49811bee81ffb5710d944ef326703d27136c05de
Carlos Martín <inean.es@gmail.com>
2023-02-13 19:58:10 +0100

    (fix): Only run macos preferences when personal flag is set

--- home/dot_config/shell/exact_zsh/editor/@interactive.zsh
+++ home/dot_config/shell/exact_zsh/editor/@interactive.zsh
@@ -8,7 +8,7 @@ export EDITOR='vim'
 # Define the default editor.
 if (( $+commands[code] )); then
   export EDITOR='code'
-  alias code="code --user-data-dir='${XDG_CONFIG_DIR}/vscode' --extensions-dir='${XDG_CACHE_DIR}/vscode'"
+  alias code="code --user-data-dir='${XDG_CONFIG_HOME}/vscode' --extensions-dir='${XDG_CACHE_HOME}/vscode'"
 fi
 export GIT_EDITOR="$EDITOR"
 export USE_EDITOR="$EDITOR"
